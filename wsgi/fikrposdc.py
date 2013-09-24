cb0a78ca465883df62e59e63ddf2f6673072af3e
swdev <swdev.bali@gmail.com>
2013-09-24 22:07:37 +0700

    fix2

--- wsgi/fikrposdc.py
+++ wsgi/fikrposdc.py
@@ -1,6 +1,25 @@
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
+
+app.config.from_pyfile('fikrposdc.cfg')
+db = SQLAlchemy(app)
+ 
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+ 
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
  
 @app.route('/')
 @app.route('/hello')
