32a04ff6cd746c363db43452f0e6a6d8617960b6
swdev <swdev.bali@gmail.com>
2013-09-24 22:02:46 +0700

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Fikr POS DC',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Centralized and distributed POS report',
+      author='Eko Wibowo',
+      author_email='swdev.bali@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask==0.7.2', 'MarkupSafe','Flask-SQLAlchemy==0.16'],
      )
