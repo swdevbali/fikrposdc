912ed1e9340ba429e719e2f2795750166221807d
swdev <swdev.bali@gmail.com>
2013-10-01 08:40:47 +0700

    make run from main.py

--- wsgi/run.py
+++
@@ -1,7 +0,0 @@
-from main import app
-if __name__ == "__main__":
-    from main import app as application
-    from main import *
-    print "Create DB"
-    db.create_all() #create db for each application startup
-    app.run(debug=True)
